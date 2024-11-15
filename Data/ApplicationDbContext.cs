137440a186aa0c8c32badf679b2bbf316091204a
OneDuckyBoy <stilianmatev@gmail.com>
2024-11-15 16:07:01 +0200

    added the project to the repository

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Portfolio.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        //public DbSet<Portfolio.Models.Project> Project { get; set; }
+    }
+}
